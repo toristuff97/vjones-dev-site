b57e5664870211b20ae14c95133f670146bd021a
Victoria Jones <66696620+toristuff97@users.noreply.github.com>
2020-08-16 10:38:07 -0400

    took link off of may heading

--- vjones-dev-site/src/Components/May.jsx
+++ vjones-dev-site/src/Components/May.jsx
@@ -6,16 +6,7 @@ export default class May extends Component {
     render() {
         return (
             <div>
-                <Link
-                    activeClass="active"
-                    to="landing"
-                    spy={true}
-                    smooth={true}
-                    offset={-50}
-                    duration={500}
-                >
-                <h1 class="may" style={{fontFamily: "Bebas Neue", fontSize: "75pt", color: "#FBFFF1", left:"40%"}}>MAY 2020</h1>
-                </Link>
+                <h1 class="may" style={{fontFamily: "Bebas Neue", fontSize: "75pt", color: "#FBFFF1", left:"30vw"}}>MAY 2020</h1>
             </div>
         )
     }
